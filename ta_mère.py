28e18efbd616245e1083bd3134aed41fbeda8196
Dorian Goepp <doriangoepp@MacBook-de-Dorian.local>
2017-07-22 23:15:25 +0200

    Preliminary work on structural code

---
+++ ta_mère.py
@@ -0,0 +1,12 @@
+# conding: utf-8
+
+class TaMère:
+    """ Find the stops near a given location
+
+        @param longitude of the point of interest
+        @param lattitude of the point of interest
+
+        @return list of stops (possibly sorted from closest to fathest)
+    """
+    find_stop(longitude, lattitude):
+        pass
