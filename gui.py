d69d49531b34d1ac2b7f8f4e9078909440746eed
Dorian Goepp <dorian.goepp@inria.fr>
2017-08-24 23:22:40 +0200

    some more docs

--- gui.py
+++ gui.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QWidget, QPushButton, QMessageBox, QApplication)
+from PyQt5.QtGui import (QIcon)
+from PyQt5.QtCore import QCoreApplication
 
 
 class Example(QWidget):
@@ -10,16 +11,57 @@ class Example(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        self.initQuit()
 
-
-    def initUI(self):
+    def initIcon(self):
 
         self.setGeometry(300, 300, 300, 220)
         self.setWindowTitle('Icon')
         self.setWindowIcon(QIcon('images/web.jpg'))
 
         self.show()
+
+    def initTooltip(self):
+
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Tooltips')
+        self.show()
+
+    def initQuit(self):
+
+        qbtn = QPushButton('Quit', self)
+        qbtn.clicked.connect(QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50, 50)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Quit button')
+        self.show()
+
+    def closeEvent(self, event):
+        """ Alter the default behaviour when closing the program.
+
+            A confirmation message is shown, allowing to keep the program open
+            if desired.
+        """
+
+        reply = QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QMessageBox.Yes |
+            QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 
 if __name__ == '__main__':
